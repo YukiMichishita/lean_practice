34f133492a0fa26ea4b5c5475fcad235b00fe448
Yuki Michishita <michishita.ocu@gmail.com>
2025-05-29 23:48:37 +0900

    aaaaaaa

--- Chap3Exercises.lean
+++ Chap3Exercises.lean
@@ -52,20 +52,102 @@ example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
             h.right.elim
                 (fun(hq : q) => show (p ∧ q) ∨ (p ∧ r) from Or.inl ⟨h.left, hq⟩)
                 (fun(hr : r) => show (p ∧ q) ∨ (p ∧ r) from Or.inr ⟨h.left, hr⟩)
-    have h₂ : (p ∧ q) ∨ (p ∧ r) → p ∧ (q ∨ r) := sorry
+    have h₂ : (p ∧ q) ∨ (p ∧ r) → p ∧ (q ∨ r) :=
+        fun (h : (p ∧ q) ∨ (p ∧ r)) =>
+            h.elim
+                (fun(h₁: (p ∧ q)) => ⟨h₁.left, Or.inl h₁.right⟩)
+                (fun(h₂: (p ∧ r)) => ⟨h₂.left, Or.inr h₂.right⟩)
     Iff.intro h₁ h₂
 
-example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) :=
+    have h₁ : p ∨ (q ∧ r) → (p ∨ q) ∧ (p ∨ r) :=
+        fun(h:p ∨ (q ∧ r)) =>
+            h.elim
+                (fun(h₂:p) => ⟨Or.inl h₂, Or.inl h₂⟩)
+                (fun(h₃:q ∧ r) => ⟨Or.inr h₃.left, Or.inr h₃.right⟩)
+    have h₂ : (p ∨ q) ∧ (p ∨ r) → p ∨ (q ∧ r) :=
+        have hl: (hl₁:p) → (p ∨ (q ∧ r)) := fun(hl₁:p) => Or.inl hl₁
+        fun(h:(p ∨ q) ∧ (p ∨ r)) =>
+            h.left.elim
+                (hl)
+                (fun(h₄:q) =>
+                    h.right.elim
+                        (hl)
+                        (fun(h₆:r) => Or.inr ⟨h₄, h₆⟩)
+                )
+    Iff.intro h₁ h₂
 
 -- 他の性質
-example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
-example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
-example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-example : ¬(p ∧ ¬p) := sorry
-example : p ∧ ¬q → ¬(p → q) := sorry
-example : ¬p → (p → q) := sorry
-example : (¬p ∨ q) → (p → q) := sorry
-example : p ∨ False ↔ p := sorry
+example : (p → (q → r)) ↔ (p ∧ q → r) :=
+    have h₁ : (p → (q → r)) → (p ∧ q → r) :=
+        fun(h₁:p → (q → r)) => (fun (h₂:p ∧ q) => h₁ h₂.left h₂.right)
+    have h₂ : (p ∧ q → r) → (p → (q → r)) :=
+        fun (h₁:p ∧ q → r) => (fun (h₂:p) => (fun(h₃:q) => h₁ ⟨h₂, h₃⟩))
+    Iff.intro h₁ h₂
+
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
+    have h₁ : ((p ∨ q) → r) → (p → r) ∧ (q → r) :=
+        fun h₁:(p ∨ q → r) =>
+            have h₂ := fun h:p => h₁ (Or.inl h)
+            have h₃ := fun h:q => h₁ (Or.inr h)
+            ⟨h₂,h₃⟩
+    have h₂ : (p → r) ∧ (q → r) →  ((p ∨ q) → r) :=
+        fun h₁: (p → r) ∧ (q → r) =>
+            fun h₂: p ∨ q =>
+                h₂.elim
+                    (fun h₃:p => h₁.left h₃)
+                    (fun h₃:q => h₁.right h₃)
+    Iff.intro h₁ h₂
+
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
+    have h₁ :=
+        fun h₂ : ¬(p ∨ q) =>
+            have np := fun h₃ : p => show False from h₂ (Or.inl h₃)
+            have nq := fun h₃ : q => show False from  h₂ (Or.inr h₃)
+            ⟨np,nq⟩
+    have h₂ :=
+        fun h₂ : ¬p ∧ ¬q =>
+            fun h₃ : p ∨ q =>
+                h₃.elim
+                    (fun h₄: p => show False from h₂.left h₄)
+                    (fun h₄: q => show False from h₂.right h₄)
+    Iff.intro h₁ h₂
+
+example : ¬p ∨ ¬q → ¬(p ∧ q) :=
+    fun h: ¬p ∨ ¬q =>
+        fun h₂: p ∧ q =>
+            h.elim
+                (fun h₃:¬p => show False from h₃ h₂.left)
+                (fun h₃:¬q => show False from h₃ h₂.right)
+
+example : ¬(p ∧ ¬p) :=
+    fun h: p ∧ ¬p =>
+        show False from h.right h.left
+
+example : p ∧ ¬q → ¬(p → q) :=
+    fun h: p ∧ ¬q =>
+        fun h₂: p → q =>
+            show False from h.right (h₂ h.left)
+
+example : ¬p → (p → q) :=
+    fun h₁: ¬p =>
+        fun h₂: p =>
+            absurd h₂ h₁
+
+example : (¬p ∨ q) → (p → q) :=
+    fun h₁: ¬p ∨ q =>
+        fun h₂: p =>
+            h₁.elim
+                (fun h₃:¬p => absurd h₂ h₃)
+                id
+
+example : p ∨ False ↔ p :=
+    have r := fun h₁: p ∨ False =>
+        h₁.elim
+            (show p → p from id)
+            (show False → p from (fun f:False => f.elim))
+    have l := fun h₁: p => Or.inl h₁
+    Iff.intro r l
+
 example : p ∧ False ↔ False := sorry
 example : (p → q) → (¬q → ¬p) := sorry
